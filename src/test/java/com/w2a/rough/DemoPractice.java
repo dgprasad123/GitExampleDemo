8192d6ca5337f5f1d387be822a3624631b896678
dgprasad123 <prasad.dpm@gmail.com>
2022-12-10 19:15:29 +0530

    final commit by asian dev team

---
+++ src/test/java/com/w2a/rough/DemoPractice.java
@@ -0,0 +1,12 @@
+package com.w2a.rough;
+
+public class DemoPractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome");
+		// TODO Auto-generated method stub
+
+	}
+
+}
